f5a5e9c9ba269b5c342e218596fe849e7cacd16b
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-02-12 11:08:57 +0100

    Changed file index.js

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-/*var http = require('http');
-
-var server = http.createServer();
-server.on('request', function (request, response) {
-    response.write('Hello world!');
-    response.end();
-});
-server.listen(9000);*/
-
 var http = require('http');
 var fs = require('fs');
-
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    
     if (request.method === 'GET' && request.url === '/') {
+
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
     	fs.readFile('./index.html', 'utf-8', function(err, data) {
-    		console.log('Zawartość pliku: ');
-    		console.log(data);
-    	});
-        response.write('<h1>Zawartość pliku html: </h1>', data);
-        response.end();
-    } else {
-            response.statusCode = 404;
-            response.write('<h1>404</h1>', '\n<img src="https://i.imgflip.com/p47u0.jpg">');
+    	
+            if (err) throw err;
+            response.write(data);
             response.end();
-    };
-});
+    	});
+        
+    } else {
+
+            response.setHeader("Content-Type", "image/jpg");
+            fs.readFile('./404.jpg', function(err, data) {
+
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        });
+            
+    }
+})
 
 server.listen(8080);
